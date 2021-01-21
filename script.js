dcf7420b3298240ad9dc163cb626aba483bbee4c
jamesdbiasi <65314809+jamesdbiasi@users.noreply.github.com>
2021-01-20 21:35:01 -0500

    Fixed underlineParent
    Only text being underline now

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var li = document.getElementsByTagName("li");
+var span = document.getElementsByTagName("span");
 
 function inputLength() {
 	return input.value.length;
@@ -9,22 +10,25 @@ function inputLength() {
 
 function createListElement() {
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
+	var span = li.appendChild(document.createElement("span"));
+	span.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
 
-	var button =document.createElement("button");
+	var button = document.createElement("button");
+	button.classList.add("btn", "btn-primary");
 	button.appendChild(document.createTextNode("Done!"));
 	li.appendChild(button);
-	button.onclick=underlineParent;
+	button.onclick = underlineSibling;
 // CREATE AN ELEMENT
-	var button =document.createElement("button"); 
+	var button = document.createElement("button");
+	button.classList.add("btn","btn-danger"); 
 // APPEND TEXT TO BUTTON
 	button.appendChild(document.createTextNode("Delete!"));
 // APPEND BUTTON TO LIST
 	li.appendChild(button);
 
-	button.onclick=removeParent;
+	button.onclick = removeParent;
 //THIS FUNCTION WILL DO THIS FOR EVERY BUTTON CREATED.
 	
 }
@@ -34,8 +38,8 @@ function createListElement() {
 // 	target.classList.toggle("done");
 // }
 
-function underlineParent(event){
-	event.target.parentNode.classList.toggle("done");
+function underlineSibling(event){
+	event.target.previousSibling.classList.toggle("done");
 }
 
 
